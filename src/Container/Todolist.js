f6d26bd49d1b5f400e632c2ef56657e4f40e0d15
max bischof <max@bischof.dk>
2020-08-11 16:43:43 +0200

    Add state to todolist

--- src/Container/Todolist.js
+++ src/Container/Todolist.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react'
 import Todo from '../Components/Todo'
 
 class Todolist extends Component {
-  // state = {
-
-  // }
+  state = {
+    todos: [
+      {title: "Eis kaufen", description: "ganz wichtig!"},
+      {title: "Neue Fische Hausaufgaben", description: "React Intro"}
+    ]
+  }
 
   render () {
+    const todos = []
+    this.state.todos.forEach((todo) => {
+      todos.push(<Todo title={todo.title} description={todo.description}/>)
+    })
+ 
     return (
-      <section className="todolist">
-        <Todo title="Eis kaufen" description="ganz wichtig!"></Todo>
-        <Todo title="Neue Fische Hausaufgaben" description="React Intro"></Todo>
+      <section>
+        {todos}
       </section>
     )
   }
