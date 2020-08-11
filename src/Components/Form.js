1d8fdbc42f692f2f72eb60157d09875f9d188559
max bischof <max@bischof.dk>
2020-08-11 21:56:27 +0200

    Remove material css

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -20,22 +20,10 @@ class Form extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-          <input
-            className="mdl-textfield__input"
-            type="text"
-            id="todo_title"
-            onChange={this.handleChange}
-          ></input>
-          <label className="mdl-textfield__label" htmlFor="todo_title">
-            Title
-          </label>
-        </div>
-
-        {/* <label>
+        <label>
           Todo Title:
           <input type="text" name="title" onChange={this.handleChange} />
-        </label> */}
+        </label>
         <input type="submit" value="Submit" />
       </form>
     )
