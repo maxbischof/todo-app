82f6782ee4f538c77c3f80acd6150dd259cfc4fc
max bischof <max@bischof.dk>
2020-08-12 16:12:42 +0200

    Change form to function and use useState

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,63 +1,51 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import "./Form.css"
 
-class Form extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { title: "", description: "" }
-    this.handleTitleChange = this.handleTitleChange.bind(this)
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+function Form({ add }) {
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
+  function handleSubmit(event) {
+    add({ title: title, description: description, done: false })
   }
 
-  handleSubmit(event) {
-    this.props.add({
-      title: this.state.title,
-      description: this.state.description,
-      done: false,
-    })
-    event.preventDefault()
+  function handleTitleChange(event) {
+    setTitle(event.target.value)
   }
 
-  handleTitleChange(event) {
-    this.setState({ title: event.target.value })
+  function handleDescriptionChange(event) {
+    setDescription(event.target.value)
   }
 
-  handleDescriptionChange(event) {
-    this.setState({ description: event.target.value })
-  }
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <div className="form_input">
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          onChange={handleTitleChange}
+          className="form_input_field"
+        />
+      </div>
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form">
-        <div className="form_input">
-          <label htmlFor="title">Title</label>
-          <input
-            id="title"
-            type="text"
-            name="title"
-            onChange={this.handleTitleChange}
-            className="form_input_field"
-          />
-        </div>
+      <div className="form_input">
+        <label htmlFor="description">Description</label>
+        <input
+          id="description"
+          type="text"
+          name="description"
+          onChange={handleDescriptionChange}
+          className="form_input_field"
+        />
+      </div>
 
-        <div className="form_input">
-          <label htmlFor="description">Description</label>
-          <input
-            id="description"
-            type="text"
-            name="description"
-            onChange={this.handleDescriptionChange}
-            className="form_input_field"
-          />
-        </div>
-
-        <button className="form_btn" type="submit" value="Submit">
-          Add todo
-        </button>
-      </form>
-    )
-  }
+      <button className="form_btn" type="submit" value="Submit">
+        Add todo
+      </button>
+    </form>
+  )
 }
 
 export default Form
