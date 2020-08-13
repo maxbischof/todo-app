280f11d49b2fdb9001d1c773d9de698b7ff3e15e
max bischof <max@bischof.dk>
2020-08-13 19:23:08 +0200

    Add /build folder fot gh-pages

---
+++ build/precache-manifest.fdfdd237677e836e1c6495e5a5434e7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccb832e31e48cba99485ba9426fe8d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c848dce681a049da361",
+    "url": "/static/js/2.27e3f9c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27e3f9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "276175c06e9eafc8f7ca",
+    "url": "/static/js/main.14d9400a.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
