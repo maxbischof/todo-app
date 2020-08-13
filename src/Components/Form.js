d42d9bf162e1fa8994db774fc153b830753388ee
max bischof <max@bischof.dk>
2020-08-13 15:32:45 +0200

    Add styled components to form and todolist

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import "./Form.css"
+import styled from 'styled-components'
 
 function Form({ add }) {
   const [title, setTitle] = useState("")
@@ -19,35 +19,85 @@ function Form({ add }) {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="form">
-      <h3>Add new todo</h3>
-      <div className="form_input">
-        <label htmlFor="title">Title</label>
-        <input
+    <StyledForm onSubmit={handleSubmit}>
+      <Headline>Add new todo</Headline>
+      <Input>
+        <Label htmlFor="title">Title</Label>
+        <InputField
           id="title"
           type="text"
           name="title"
           onChange={handleTitleChange}
-          className="form_input_field"
         />
-      </div>
+      </Input>
 
-      <div className="form_input">
-        <label htmlFor="description">Description</label>
-        <input
+      <Input>
+        <Label htmlFor="description">Description</Label>
+        <InputField
           id="description"
           type="text"
           name="description"
           onChange={handleDescriptionChange}
-          className="form_input_field"
         />
-      </div>
+      </Input>
 
-      <button className="form_btn" type="submit" value="Submit">
+      <Button type="submit" value="Submit">
         Add todo
-      </button>
-    </form>
+      </Button>
+    </StyledForm>
   )
 }
 
 export default Form
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px 20px 20px 20px;
+  margin: 0 0 20px 0;
+  background-color: white;
+  border-radius: 20px;`
+
+const Button = styled.button`
+text-decoration: none;
+color: #fff;
+background-color: #26a69a;
+text-align: center;
+border: none;
+border-radius: 2px;
+height: 36px;
+line-height: 36px;
+padding: 0 16px;
+margin: 20px 0px 0px 0px;
+text-transform: uppercase;
+text-align: center;`
+
+const InputField = styled.input`
+  background-color: transparent;
+  border: none;
+  border-bottom-color: currentcolor;
+  border-bottom-style: none;
+  border-bottom-width: medium;
+  border-bottom: 1px solid #9e9e9e;
+  border-radius: 0;
+  outline: none;
+  width: 100%;
+  font-size: 16px;
+  padding: 0;
+  height: 40px;`
+
+const Input  = styled.div`
+  position: relative;
+  margin-bottom: 15px;
+  height: 40px;`
+
+const Label = styled.label`
+  position: absolute;
+  bottom: -17px;
+  left: 2px;
+  color: #9e9e9e;
+  font-size: 14px;`
+
+const Headline = styled.h3`
+  margin: 0 0 10px 0;`
--- src/Container/Todolist.js
+++ src/Container/Todolist.js
@@ -1,10 +1,10 @@
 import React from "react"
 import Todo from "../Components/Todo"
-import './Todolist.css'
+import styled from 'styled-components'
 
 const Todolist = ({ todos, deleteTodo, changeDone }) => {
   const sortedTodos = todos.sort((a,b) => a.done - b.done)
-  return <section className="todolist">
+  return <List>
     {sortedTodos.map((todo, index) => 
     <Todo
     key={todo.title}
@@ -15,7 +15,14 @@ const Todolist = ({ todos, deleteTodo, changeDone }) => {
     index={index}
     changeDone={changeDone}
   />)}
-  </section>
+  </List>
 }
 
 export default Todolist
+
+const List = styled.section`
+  border-top-left-radius:  20px;
+  border-top-right-radius:  20px;
+  background-color: white;
+  place-items: center;
+  width: 100%`
